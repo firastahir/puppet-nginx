10e35a23c2fc74cbe0d6ecc95326607c75e119e7
root <root@azuredbbestcom1.mylabserver.com>
2017-12-23 05:36:19 +0000

    Another update

---
+++ manifests/install.pp
@@ -0,0 +1,12 @@
+class nignix::install (
+  String $ensure =  $::nginx::ensure,
+  String $package_name = $::nginx::package_name,
+){
+  package {'install nginx':
+    ensure => $ensure,
+    name   => $package_name,
+
+  }
+
+
+}
